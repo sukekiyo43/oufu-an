0c938a0914823386d118aca9cf435e6872b81880
sukekiyo43 <teihen13@infoseek.jp>
2024-11-13 01:27:10 +0900

    日本語ヘッダーSPまで完了

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    function initializeSlick(){
+$(document).ready(function () {
+    // MVスライドの設定
+    function initializeSlick() {
         if ($(window).width() >= 768) {
             if (!$('.slide-items').hasClass('slick-initialized')) {
                 $('.slide-items').slick({
@@ -7,7 +8,7 @@ $(document).ready(function(){
                     autoplaySpeed: 5000,
                     fade: true,
                     arrows: false,
-                    speed: 3000 // フェードの速度を1秒に設定
+                    speed: 3000
                 });
             }
             if ($('.slide-items-sp').hasClass('slick-initialized')) {
@@ -20,7 +21,7 @@ $(document).ready(function(){
                     autoplaySpeed: 5000,
                     fade: true,
                     arrows: false,
-                    speed: 3000 // フェードの速度を1秒に設定
+                    speed: 3000
                 });
             }
             if ($('.slide-items').hasClass('slick-initialized')) {
@@ -31,4 +32,48 @@ $(document).ready(function(){
 
     initializeSlick();
     $(window).resize(initializeSlick);
+
+    // スクロールで要素をフェードインさせる設定
+    $(".fade, .fade-right, .fade-left").on("inview", function () {
+        $(this).addClass("fade-in");
+    });
+
+    // トップに戻るボタンの設定
+    $(window).on('scroll', function() {
+        let scrollPosition = $(window).scrollTop();
+
+        if (scrollPosition > 600) {
+            $('.top-button').addClass('top-button-visible');
+        } else {
+            $('.top-button').removeClass('top-button-visible');
+        }
+    });
+
+    $('.top-button').on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 600);
+    });
+
+    // ハンバーガーメニュー開閉の設定
+    $('.menu-button').on('click', function () {
+        $('.menu-box').toggleClass('open');
+        $('body').toggleClass('open');
+    });
+
+    let resizeTimer;
+
+    function checkWidth() {
+        if ($(window).outerWidth(true) >= 768) {
+            $('.menu-box').removeClass('open');
+            $('body').removeClass('open');
+        }
+    }
+
+    $(window).on('resize', function () {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(checkWidth, 100);
+    });
+
+    $(window).on('load', checkWidth);
+
 });
